e6a185cfe57460bfbfad90f7748efc19aa26c3f7
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2017-10-15 18:45:28 +0200

    The plant grows

--- RV01/Assets/Scripts/Plant.cs
+++ RV01/Assets/Scripts/Plant.cs
@@ -8,6 +8,10 @@ public class Plant : MonoBehaviour {
     protected Soil soil;
     // The optimal level of humidity to grow.
     protected float optimalHumidity;
+    // The growth speed.
+    protected float growthSpeed;
+    // The growth progress.
+    protected float growthProgress;
 
     public Soil Soil
     {
@@ -35,13 +39,63 @@ public class Plant : MonoBehaviour {
         }
     }
 
+    public float GrowthSpeed
+    {
+        get
+        {
+            return growthSpeed;
+        }
+
+        set
+        {
+            growthSpeed = value;
+        }
+    }
+
+    public float GrowthProgress
+    {
+        get
+        {
+            return growthProgress;
+        }
+
+        set
+        {
+            growthProgress = value;
+        }
+    }
     // Use this for initialization
     void Start () {
         this.Soil = null;
+        this.growthProgress = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        this.Grow();
 	}
+
+    // The plant grows.
+    private void Grow()
+    {
+        // The values of humidity needed.
+        float minHumidityRequired = this.optimalHumidity * 0.9f;
+        float maxHumidityRequired = this.optimalHumidity * 1.1f;
+
+        // If the soil is wet enough.
+        if (this.soil.HumidityLevel > minHumidityRequired && this.soil.HumidityLevel < maxHumidityRequired)
+        {
+            this.growthProgress += this.growthSpeed;
+            if (this.growthProgress > 1)
+            {
+                this.growthProgress = 1;
+            }
+        }
+    }
+
+    // Over ?
+    private bool IsOver()
+    {
+        return this.growthProgress >= 1;
+    }
 }
