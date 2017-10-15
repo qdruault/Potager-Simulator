064b7901b3754072348981603f64a23977cbcb03
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2017-10-15 17:59:11 +0200

    Create Soil script

---
+++ RV01/Assets/Scripts/Soil.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soil : MonoBehaviour {
+
+    private enum SoilType { Earth, Sand };
+
+    private float humidityLevel;
+    private SoilType type;
+
+    private SoilType Type
+    {
+        get
+        {
+            return type;
+        }
+
+        set
+        {
+            type = value;
+        }
+    }
+
+    public float HumidityLevel
+    {
+        get
+        {
+            return humidityLevel;
+        }
+
+        set
+        {
+            humidityLevel = value;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
