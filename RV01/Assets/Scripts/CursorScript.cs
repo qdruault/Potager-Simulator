3893ed386159875c53e9fa74e9d098a6d8fbf712
Quentin DRUAULT-AUBIN <quentin.druault-aubin@etu.utc.fr>
2017-11-19 22:59:30 +0100

    [Slider] Add bounds to the slider

--- RV01/Assets/Scripts/CursorScript.cs
+++ RV01/Assets/Scripts/CursorScript.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class CursorScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Bounds of the cursor.
+    private float minX;
+    private float maxX;
+
+
+    // Use this for initialization
+    void Start () {
+        // Get the bounds relative to the Rail position and size.
+        Transform railTransform = GameObject.Find("Rail").transform;
+        minX = railTransform.position.x - railTransform.localScale.x / 2 + transform.localScale.x / 2;
+        maxX = railTransform.position.x + railTransform.localScale.x / 2 - transform.localScale.x / 2;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        // Block the slider.
+        if(transform.position.x < minX)
+        {
+            transform.position = new Vector3(minX, transform.position.y, transform.position.z);
+        } else if (transform.position.x > maxX)
+        {
+            transform.position = new Vector3(maxX, transform.position.y, transform.position.z);
+        }
+    }
 }
