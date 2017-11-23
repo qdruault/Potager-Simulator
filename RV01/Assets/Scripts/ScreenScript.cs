7b18a8b4243c267e3272829590ba96e5f362adc9
Baptiste De Filippis <baptiste.de-filippis@etu.utc.fr>
2017-11-23 10:04:37 +0100

    Ecran de controle

---
+++ RV01/Assets/Scripts/ScreenScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenScript : MonoBehaviour {
+
+	public GameObject temperatureTB;
+	public GameObject illuminationTB;
+
+	private Text temperatureText;
+	private Text illuminationText;
+
+	private string temperature;
+	private string illumination;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Update the temperature.
+		temperature = GameObject.Find("Cursor").GetComponent<CursorScript>().Temperature.ToString();
+		temperatureText = temperatureTB.GetComponent<Text>();
+		temperatureText.text = temperature;
+			
+		// Update the illumination.
+		illumination = GameObject.Find("RotatorButton").GetComponent<RotatorButtonScript>().Illumination.ToString();
+		illuminationText = illuminationTB.GetComponent<Text>();
+		illuminationText.text = illumination;
+	}
+}
